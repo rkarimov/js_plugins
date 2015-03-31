6da92dac870a3028f98e853d505e09e9112f6427
Sergey Myasnoy <dry.swimer@gmail.com>
2015-03-31 14:04:41 +0300

    formattig fix

--- carousel/js/main.js
+++ carousel/js/main.js
@@ -1,26 +1,26 @@
 (function($){
-	$(document).ready(function(){
-		$('#slide-show-1').carousel({
-			slides:['images/image_1.png',
-					'images/image_2.png',
-					'images/image_3.png'],
-			slideWidth: 450,
-			interval: 5000,
-		});
-		
-		$('#slide-show-2').carousel({
-			slides:['images/image_3.png',
-					'images/image_2.png',
-					'images/image_1.png'],
-			slideWidth: 450,
-			interval: 2500,
-		});
-		$('#slide-show-3').carousel({
-			slides:['images/image_2.png',
-					'images/image_3.png',
-					'images/image_1.png'],
-			slideWidth: 450,
-			interval: 1000,
-		});
-	});
+    $(document).ready(function(){
+        $('#slide-show-1').carousel({
+            slides:['images/image_1.png',
+                    'images/image_2.png',
+                    'images/image_3.png'],
+            slideWidth: 450,
+            interval: 5000,
+        });
+        
+        $('#slide-show-2').carousel({
+            slides:['images/image_3.png',
+                    'images/image_2.png',
+                    'images/image_1.png'],
+            slideWidth: 450,
+            interval: 2500,
+        });
+        $('#slide-show-3').carousel({
+            slides:['images/image_2.png',
+                    'images/image_3.png',
+                    'images/image_1.png'],
+            slideWidth: 450,
+            interval: 1000,
+        });
+    });
 })(jQuery)
